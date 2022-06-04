6497afae49466f368559b1fc92de5d5208d92030
0som <0something0sn@gmail.com>
2022-06-04 13:21:28 -0400

    Better processing of input image values

--- generate-dataset.py
+++ generate-dataset.py
@@ -7,7 +7,7 @@
 import numpy as np
 import imagecodecs
 import rasterio
-from numpy import float32, ndarray
+from numpy import NaN, float32, ndarray
 
 import keras
 from keras import layers
@@ -18,15 +18,27 @@ MAP_NAMES = ['temp', 'rain', 'elev']
 MAX_VALUES = {'temp':40, 'rain':3000, 'elev':7000}
 
 #Reads from f'{filename}_map.tif', clamps to min and max, and removes NaNs'
-#seems to be lower this way, better compare between processing input before and after splitting
+#seems to be slower this way, better compare between processing input before and after splitting
+# filename - string, name of file to read, relative directory
 def readfile(filename: str, min: int, max: int):
     
     if filename.find('.tif') != -1:
-        arr =  np.array(rasterio.open(filename).read())[0]
+        tif = rasterio.open(filename)
+        arr = np.array(tif.read(), dtype=np.float32)[0]
+        tags = tif.tags(1)
+
+        #get min/max from metadata
+        if 'STATISTICS_MINIMUN' in tags: min = float(tags['STATISTICS_MINIMUN'])
+        if 'STATISTICS_MAXIMUM' in tags: max = float(tags['STATISTICS_MAXIMUM'])
+
+        #for each element, if in tif.nodatavals, set to NaN
+        for value in tif.nodatavals:
+            arr[arr==value] = np.nan
+
     elif filename.find('.png') != -1:
         arr = imagecodecs.imread(filename)
 
-    arr = np.nan_to_num(arr)
+    #arr = np.nan_to_num(arr)
     arr = np.clip(arr, min, max)
     arr = (arr - min)/max
     return arr
@@ -39,12 +51,22 @@ def readfile(filename: str, min: int, max: int):
 def split_into_chunks(chunk_size: int, input_arrays: list) -> list:
 
     #prepare list of resulting chunks - each ndarray has n of 2D chunks, and extra dimensions for potential subpixels
+    # see: broadcasting
     input_chunks = [np.zeros((1, chunk_size, chunk_size) + arr.shape[2:], dtype=float32) for arr in input_arrays]
 
     #reduced sample size for test purposes, should be len(input_arrays['temp']) and len(input_arrays['temp'][i]
-    row_start = int(len(input_arrays[0]) / 2)
-    col_start = int(len(input_arrays[0][1]) / 2 + 512)
+    #row_start = int(len(input_arrays[0]) / 2)
+    #col_start = int(len(input_arrays[0][1]) / 2 + 512)
+    
+    #should be pointed off the Southeastern United States
+    #if using a 4320x2160 equirectengular map, centered at long/lat (0, 0)
 
+    #y-coord/latitude
+    row_start = 690 
+    #x-coord/longitude
+    col_start = 1130
+
+    #cauess issues with row = input_arrays[0]) / 2 + 192
     for row in range(row_start, row_start + 128, chunk_size):
         for col in range(col_start, col_start + 128, chunk_size):
 
@@ -66,13 +88,21 @@ def split_into_chunks(chunk_size: int, input_arrays: list) -> list:
 
 # flatten and process data for training
 def flatten_input(chunks: ndarray) -> ndarray:
+
     #flatten from (1, x, x, ...) to (1, x^2...)    
     chunks = chunks.reshape(chunks.shape[0], np.prod(chunks.shape[1:])) 
     
+    #replace NaN with mean values
+    #for count, chunk in enumerate(chunks):
+    for i in range(len(chunks)):
+        chunk = chunks[i]   
+        mean = np.nanmean(chunk)
+
+        chunks[i] = np.where(np.isnan(chunk), mean, chunk)
+
     assert np.all(chunks >= 0)
     assert np.all(chunks <= 1)
     return chunks
-
 
 #set up convolutional neural network model
 def build_model():
@@ -107,7 +137,6 @@ colorarray = readfile(r'colormap.png', 0 ,255)
 
 temp_chunks, rain_chunks, elev_chunks, colorchunks = \
     [flatten_input(chunks) for chunks in split_into_chunks(CHUNK_SIZE, list(input_arrays.values()) + [colorarray])]
-
 
 print(f'{len(temp_chunks)} {len(rain_chunks)} {len(elev_chunks)} {len(colorchunks)}')
 
